cef45bc34be801c21fae0571e88ed509df0d7576
Thanh Thanh <tth0704@gmail.com>
2023-07-05 16:41:43 +0700

    update

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const cors = require('cors');
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser')
 const emailRoute = require('./router/email')
-const {accessWebsite} = require('./getcookie');
 const app = express();
 app.use(cors());
 app.use(morgan());
@@ -11,15 +10,6 @@ app.use(cookieParser());
 
 app.use(express.json());
 app.use("/v1", emailRoute)
-app.use("/v2", async (req, res) =>{
-  accessWebsite()
-  .then((result) =>{
-    res.status(200).json(result)
-  })
-  .catch((error)=>{
-    res.status(500).json(error)
-  })
-})
 
 app.listen(process.env.PORT || 3000, ()=>{
   console.log('Server is running')
